03ba288935425b9a02c8b5d571ddace75ee5789f
Asdasdone <tohm69@gmail.com>
2020-09-23 14:37:51 +0200

    Add project files.

---
+++ vizsgafeladat/termek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizsgafeladat
+{
+    class termek
+    {
+		private int kod;
+
+		public int KOD
+		{
+			get { return kod; }
+			set { kod = value; }
+		}
+
+	}
+}
