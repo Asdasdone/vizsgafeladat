dcc5091c6170d0eff4677b83cada100b0e20ba49
Asdasdone <tohm69@gmail.com>
2020-09-25 09:05:09 +0200

    megrendelés tételek nélkül

--- vizsgafeladat/Program.cs
+++ vizsgafeladat/Program.cs
@@ -10,6 +10,7 @@ namespace vizsgafeladat
     class Program
     {
         static List<termek> l = new List<termek>();
+        static List<rendeles> rend = new List<rendeles>();
         static void beolvas()
         {
             StreamReader ol = new StreamReader("raktar.csv");
@@ -22,6 +23,23 @@ namespace vizsgafeladat
             }
             ol.Close();
         }
+        static void rendelol()
+        {
+            StreamReader ol = new StreamReader("rendeles.csv");
+            while (!ol.EndOfStream)
+            {
+                string[] sor = ol.ReadLine().Split(';');
+
+                if (sor[0] == "M")
+                {
+                    rend.Add(new rendeles(sor[0], sor[1], sor[2], sor[3]));
+                }
+
+                
+
+            }
+            ol.Close();
+        }
         static void Main(string[] args)
         {
 
@@ -30,8 +48,11 @@ namespace vizsgafeladat
              {
                  Console.WriteLine($"{l[i].KOD},{l[i].NEV},{l[i].AR},{l[i].DB}");
              }
-
-            
+            rendelol();
+            for (int i = 0; i < l.Count; i++)
+            {
+                Console.WriteLine($"{rend[i].DATUM},{rend[i].SORSZAM},{rend[i].EMAIL}");
+            }
             Console.ReadKey();
             
         }
---
+++ vizsgafeladat/rendeles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vizsgafeladat
+{
+    class rendeles
+    {
+        private string tipus, datum, sorszam, email;
+        
+
+        public string TIPUS
+        {
+            get { return tipus; }
+            set { tipus = value; }
+        }
+        
+
+        public string DATUM
+        {
+            get { return datum; }
+            set { datum = value; }
+        }
+        
+
+        public string SORSZAM
+        {
+            get { return sorszam; }
+            set { sorszam = value; }
+        }
+        
+
+        public string EMAIL
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+
+
+
+        public rendeles(string tipus,string datum,string sorszam,string email)
+        {
+            this.tipus = tipus;
+            this.datum = datum;
+            this.sorszam = sorszam;
+            this.email = email;
+        }
+    }
+}
